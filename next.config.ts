1d95f8e2b311562b7ea205ba05053038983540a2
NadaAhme <nadaahmed33470@gmail.com>
2025-01-16 23:36:05 +0200

    files add

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true, // تجاهل أخطاء TypeScript أثناء البناء
+  },
+  /* يمكنك إضافة خيارات أخرى هنا */
 };
 
 export default nextConfig;
