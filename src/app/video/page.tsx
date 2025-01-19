7dd28f11f7fb8aac37b948900c7d1166e33a5b11
NadaAhme <nadaahmed33470@gmail.com>
2025-01-20 00:27:09 +0200

    delete video agai

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -9,7 +9,7 @@ function Video() {
   const { t } = useTranslation();
 
   return (
-    <div className="video-gallery bg-[#333333] pb-5 h-full" id='video'>
+    <div className="video-gallery bg-[#333333] pb-5 h-screen" id='video'>
       <h3 className="text-white text-5xl font-normal flex justify-center items-center p-5" style={{ wordSpacing: "-10px" }}>
         <span className="text-yellow-600 font-bold">{t("video-tag1")}</span> {t("video-tag2")}
       </h3>
