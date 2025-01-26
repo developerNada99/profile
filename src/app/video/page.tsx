e6e6c4c42cdc544198700f8824c508dad26eb71a
NadaAhme <nadaahmed33470@gmail.com>
2025-01-26 22:38:07 +0200

    delete om el translate

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import { useTranslation } from "react-i18next";
-
 const Video = () => {
-  const { t, i18n } = useTranslation();
 
   // التحقق من تحميل الترجمة
-  if (!i18n.isInitialized) {
-    return <div>Loading...</div>; // يمكن تخصيص نص التحميل حسب الحاجة
-  }
+  
 
   return (
     <div>
-      <h1>{t("tag1-video")}</h1>
+      <h1>ggggggggggggggg</h1>
     </div>
   );
 };
