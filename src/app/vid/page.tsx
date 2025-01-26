5207941405a84322480ae9c4033ad5f96f096918
NadaAhme <nadaahmed33470@gmail.com>
2025-01-26 02:39:55 +0200

    add translan

--- src/app/vid/page.tsx
+++ src/app/vid/page.tsx
@@ -1,37 +1,26 @@
 "use client";
-
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 function VideoGallery() {
-  const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [selectedVideo, setSelectedVideo] = useState("HV0JS4WiM4k");
   const { t } = useTranslation();
 
-  useEffect(() => {
-    // Fetch videos dynamically
-    async function fetchVideos() {
-      const fetchedVideos = [
-        { title: "Tag 1 Video", id: "HV0JS4WiM4k" },
-        { title: "Tag 2 Video", id: "VFYoStUWML0" },
-        { title: "Tag 3 Video", id: "vB3oAdgKW70" },
-        { title: "Tag 4 Video", id: "Mo1l9bEql7Q" },
-        { title: "Tag 5 Video", id: "TCwgfOrFYKU" },
-        { title: "Tag 6 Video", id: "t34SRDYtTiQ" },
-        { title: "Tag 7 Video", id: "rvonT2Ws21M" },
-        { title: "Tag 8 Video", id: "DmVzHkngdMs" },
-        { title: "Tag 9 Video", id: "PTcRAovVpKE" },
-        { title: "Tag 10 Video", id: "7pS2-EsE1Vw" },
-        { title: "Tag 11 Video", id: "OAyVg-mua1s" },
-        { title: "Tag 12 Video", id: "jogRfmcAudM" },
-        { title: "Tag 13 Video", id: "OiYFabeN-ls" },
-      ];
-      setVideos(fetchedVideos);
-      setSelectedVideo(fetchedVideos[0]?.id || null); // Set the first video as default
-    }
-
-    fetchVideos();
-  }, []);
+  const videos = [
+    { title: t("tag1-video"), id: "HV0JS4WiM4k" },
+    { title: "Tag 2 Video", id: "VFYoStUWML0" },
+    { title: "Tag 3 Video", id: "vB3oAdgKW70" },
+    { title: "Tag 4 Video", id: "Mo1l9bEql7Q" },
+    { title: "Tag 5 Video", id: "TCwgfOrFYKU" },
+    { title: "Tag 6 Video", id: "t34SRDYtTiQ" },
+    { title: "Tag 7 Video", id: "rvonT2Ws21M" },
+    { title: "Tag 8 Video", id: "DmVzHkngdMs" },
+    { title: "Tag 9 Video", id: "PTcRAovVpKE" },
+    { title: "Tag 10 Video", id: "7pS2-EsE1Vw" },
+    { title: "Tag 11 Video", id: "OAyVg-mua1s" },
+    { title: "Tag 12 Video", id: "jogRfmcAudM" },
+    { title: "Tag 13 Video", id: "OiYFabeN-ls" },
+  ];
 
   const handleVideoClick = (id) => {
     setSelectedVideo(id);
@@ -43,8 +32,7 @@ function VideoGallery() {
         className="text-white text-5xl font-normal flex justify-center items-center p-5"
         style={{ wordSpacing: "-10px" }}
       >
-        <span className="text-yellow-600 font-bold">{t("tag1-video")}</span>{" "}
-        Gallery
+        <span className="text-yellow-600 font-bold">{t("tag1-video")}</span> {t("gallery")}
       </h3>
       <div className="flex justify-start items-center max-md:flex-col">
         <div
@@ -53,11 +41,7 @@ function VideoGallery() {
         >
           <ul>
             {videos.map((video, index) => (
-              <li
-                key={index}
-                className="mb-4 cursor-pointer"
-                onClick={() => handleVideoClick(video.id)}
-              >
+              <li key={index} className="mb-4 cursor-pointer" onClick={() => handleVideoClick(video.id)}>
                 <div className="flex items-center gap-4">
                   <img
                     src={`https://img.youtube.com/vi/${video.id}/0.jpg`}
@@ -71,21 +55,16 @@ function VideoGallery() {
           </ul>
         </div>
 
-        {/* Video Display Section */}
         <div className="video-display w-2/3 p-4 max-md:w-full">
-          {selectedVideo ? (
-            <iframe
-              width="100%"
-              height="500"
-              src={`https://www.youtube.com/embed/${selectedVideo}`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Selected Video"
-            ></iframe>
-          ) : (
-            <p className="text-white">No video selected</p>
-          )}
+          <iframe
+            width="100%"
+            height="500"
+            src={`https://www.youtube.com/embed/${selectedVideo}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Selected Video"
+          ></iframe>
         </div>
       </div>
     </div>
