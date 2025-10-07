142258c4d1f651f616f17eb54b138280f23a704f
NadaAhme <nadaahmed33470@gmail.com>
2025-10-07 09:42:44 +0300

    ed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,19 +21,18 @@ const Contact = () => {
 
   if (!showFooter) return null;
 
-  const iconClass =
-    "text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-white transition-transform duration-300 hover:scale-110";
+  const iconClass = "text-lg text-white transition-transform duration-300 hover:scale-110";
 
   return (
     <div
       id="contact"
-      className="p-7 bg-[#222222] flex justify-center items-center gap-4 sm:gap-6 px-6 sm:px-10"
+      className="p-6 bg-[#222222] flex justify-center items-center gap-4 px-8"
     >
       <Link
         href="https://www.instagram.com/eng.khalifaalsaadi?igsh=YmVvM2R4aGE0d3p4"
         target="_blank"
       >
-        <div className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 bg-[#333333] rounded-full flex justify-center items-center hover:bg-yellow-600 transition duration-300">
+        <div className="w-9 h-9 bg-[#333333] rounded-full flex justify-center items-center hover:bg-yellow-600 transition duration-300">
           <FaInstagram className={iconClass} />
         </div>
       </Link>
@@ -42,7 +41,7 @@ const Contact = () => {
         href="https://www.youtube.com/channel/UCM_n4g1tdyvGz5mob_3u6lQ"
         target="_blank"
       >
-        <div className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 bg-[#333333] rounded-full flex justify-center items-center hover:bg-yellow-600 transition duration-300">
+        <div className="w-9 h-9 bg-[#333333] rounded-full flex justify-center items-center hover:bg-yellow-600 transition duration-300">
           <FaYoutube className={iconClass} />
         </div>
       </Link>
@@ -51,7 +50,7 @@ const Contact = () => {
         href="https://www.linkedin.com/in/khalifa-operations-engineer?originalSubdomain=ae"
         target="_blank"
       >
-        <div className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 bg-[#333333] rounded-full flex justify-center items-center hover:bg-yellow-600 transition duration-300">
+        <div className="w-9 h-9 bg-[#333333] rounded-full flex justify-center items-center hover:bg-yellow-600 transition duration-300">
           <FaLinkedin className={iconClass} />
         </div>
       </Link>
@@ -60,7 +59,7 @@ const Contact = () => {
         href="https://www.tiktok.com/@eng.khalifaalsaadi?_t=8meE5X2kS3M&_r=1"
         target="_blank"
       >
-        <div className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 bg-[#333333] rounded-full flex justify-center items-center hover:bg-yellow-600 transition duration-300">
+        <div className="w-9 h-9 bg-[#333333] rounded-full flex justify-center items-center hover:bg-yellow-600 transition duration-300">
           <FaTiktok className={iconClass} />
         </div>
       </Link>
@@ -69,7 +68,7 @@ const Contact = () => {
         href="https://www.snapchat.com/add/eng.khalifa2024?invite_id=3HtNpI6d&locale=en_AE&share_id=DVmmOdNWTqu3REglG9blqA&sid=849b618a579642dbb8b79d9fb6f0a3e8"
         target="_blank"
       >
-        <div className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 bg-[#333333] rounded-full flex justify-center items-center hover:bg-yellow-600 transition duration-300">
+        <div className="w-9 h-9 bg-[#333333] rounded-full flex justify-center items-center hover:bg-yellow-600 transition duration-300">
           <FaSnapchatGhost className={iconClass} />
         </div>
       </Link>
